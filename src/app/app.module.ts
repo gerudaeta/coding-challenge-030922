566338710e22e9dfa169583b3f9b76687ca34817
gerudaeta <gerudaeta@gmail.com>
2022-03-11 01:33:02 -0300

    add angular material, component timer and table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import {TodoModule} from "./todo/todo.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {TodoModule} from "./todo/todo.module";
     imports: [
         BrowserModule,
         HttpClientModule,
-        TodoModule
+        TodoModule,
+        BrowserAnimationsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TimerComponent } from './componentes/timer/timer.component';
+import { TableComponent } from './componentes/table/table.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TimerComponent,
+    TableComponent
+  ],
   imports: [
     CommonModule
   ]
